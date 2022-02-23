ce0011030755ec453bc9577dc4a790e8fe6faef8
Pere Antoni Bennassar <pereantonibennassar@gmail.com>
2022-02-23 08:36:35 +0100

    exam

--- consumApi/consumAPI.py
+++ consumApi/consumAPI.py
@@ -7,10 +7,10 @@ import operator
 
 #clients_sort = sorted(clients.items(), key=operator.itemgetter(1), reverse=True)
 #cream el diccionari a partir del consum de l'api de gimnesia
-
+print("a")
 resp = requests.get('http://api.gimnesia.net/especie/read.php')
 person_dict = json.loads(resp.content)
-
+print("a")
 #person_dict = sorted(person_dict.records(),key=operator.itemgetter(1),reverse=False)
 #person_dict = sorted(person_dict)
 #print(person_dict)
---
+++ problemes/prova-python.py
@@ -0,0 +1,46 @@
+
+# -*- coding: utf-8 -*-
+cadena = ['Aglà', 'Granot arbori', 'Bolet de fems o de tinta', 'Corb marí', 'Zooplàncton', 'Ferrassa']
+cadena2 = [1,4,5,6,7,3]
+nom =  'Corb marí'
+
+
+def cercaMax(cadena):
+    max=0
+    for i in cadena:
+        if max < i:
+            max = i
+    return max
+
+def cercaMax2(cadena):
+    m = cadena[0]
+    p = 0
+    z = 0
+    for i in cadena:
+        if m < i:
+            m = i
+            z = p
+        p += 1        
+    return z
+
+#trobar una cadena
+a = 0
+for i in cadena:
+    if i == nom:
+        print(a)
+    a +=1
+
+#trobar una cadena
+a = 5
+for i in cadena:
+    if i == nom:
+        print(a)
+    a +=2
+
+a = 0
+for i in cadena:
+    print("bon dia")
+
+a = 0
+print(cercaMax2(cadena), "---",cercaMax(cadena2))
+
